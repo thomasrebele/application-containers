cac7ed660c79b395f985f12d5b1622480dde8b76
Thomas Rebele <thomas.rebele@gmail.com>
2025-06-07 11:47:15 +0200

    add user to thinbase image

--- container-base.nix
+++ container-base.nix
@@ -6,18 +6,34 @@ let
     root = ./addendum;
     fileset = ./addendum;
   };
+  myFakeNss = pkgsLinux.fakeNss.override {
+    extraPasswdLines = [''
+      act-user:x:999:999::/home/act-user:/bin/sh
+    ''];
+    extraGroupLines = [''
+      act-group:x:999:
+    ''];
+  };
 in 
 with pkgsLinux; pkgs.dockerTools.buildImage {
   name = "thinbase";
   tag = "latest";
   copyToRoot = [
-    fakeNss
+    myFakeNss
     bashInteractive
     coreutils
     busybox
     dbus
     addendum
+    shadow
   ];
+  # do not use useradd, as /etc/passwd and /etc/group are managed by fakeNss
+  runAsRoot = ''
+    ${pkgs.dockerTools.shadowSetup}
+    mkdir -p /home/act-user
+    chown act-user:act-group /home/act-user
+    chmod a+rw /tmp
+  '';
   config = {
     Cmd = [ "${pkgsLinux.bashInteractive}/bin/bash" ];
   };
